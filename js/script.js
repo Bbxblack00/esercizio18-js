1e9e814dda50bc857ed07c87458c68dd44c3a902
Benito <bbxblack@gmail.com>
2021-04-07 10:58:58 +0200

    esercizio live

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
+function initVue() {
+  new Vue({
+
+    el: '#app',
+    data: {
+
+      'title': 'hello world!',
+      'pathImg': 'img/Ciao.png',
+      'img': false,
+      'names': [
+        ['Guybrush'],
+        'Giorgio',
+        'Francesca'
+      ],
+      'factor': 1.7,
+      'img': [
+        'img/img1.jpg',
+        'img/img2.jpg',
+        'img/img3.jpg'
+      ]
+
+    },
+    methods: {
+      flipImg: funcrion() {
+        this.img = !this.img;
+      }
+    }
+  });
+}
+
 function init() {
 
-  
+  initVue();
 
 }
 
 $(init);
+
+let x = true;
+
+let z = x ? 1 : -1;
